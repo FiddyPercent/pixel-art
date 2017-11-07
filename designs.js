beaa393e5f7f80156b6d7df39a855d2d7d234715
Ashly Berry <ashlyberry@Ymail.com>
2017-11-06 22:13:09 -0700

    added variables for submit, height, width, and color

--- designs.js
+++ designs.js
@@ -2,15 +2,22 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var height, width, sButton, color;
+
+height = $('#input_height');
+width = $('#input_width');
+sButton = $(':submit');
+color = $('#colorPicker');
+
 var grid = $('#grid');
 
 function makeGridPink() { 
  grid.css('background-color', 'pink');
- alert('mouse enter');
+ //alert('mouse enter');
 }
 function makeGridOrange() { 
  grid.css('background-color', 'orange');
-  alert('mouse leave');
+  //alert('mouse leave');
 }
 //grid.css('background-color', 'orange');
 
