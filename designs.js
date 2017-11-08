2cb1e43e14bb6b22348b79c7924875bb067c9947
Ashly Berry <ashlyberry@Ymail.com>
2017-11-07 18:28:42 -0700

    added ability to change color of tiles

--- designs.js
+++ designs.js
@@ -12,29 +12,38 @@ sform = $('#sizePicker');
 loads = 0;
 
 
-
-
  function makeGrid() {
 	var cellSize, gridBorder, cellBorder, grid, tCellHeight, tCellWidth;
 	
+	//Clears Old Grid
 	grid =  $('#grid');
 	grid.children().remove();
 	gridBorder = grid.css("border-left-width");
 	
 	for(var h = height.val() - 1 ; h >= 0; h-- ){
-		$('#grid').append("<div id=\"" + h + "\"class=\"height cell\"> "+ h +" </div>");
+		$('#grid').append("<div id=\"" + h + "\"class=\"height cell\"> </div>");
+			$("#" + h).click(function() {
+				$(this).css("background-color", color.val());
+			  return false;
+			});
+		
 		for(var w = width.val() - 2; w >= 0; w-- ){
-			$('#grid').append("<div id=\"" + h + "-" + w + "\" class=\"width cell\"> " + w + " </div>");
+			$('#grid').append("<div id=\"" + h + "-" + w + "\" class=\"width cell\"> </div>");
+			$("#" + h + "-" + w).click(function() {
+				$(this).css("background-color", color.val());
+			  return false;
+			});
 		}
 	} 
 	
+	//Adjusts Grid Border Based on Size
 	cellSize = $('.cell').height();
 	cellBorder = $('.cell').css("border-left-width").replace("px","") * 2;
 	tCellHeight = ( cellSize + cellBorder )* height.val();
 	tCellWidth = ( cellSize + cellBorder )* width.val();;
 	console.log(cellSize + " = Total Cell Size " + cellBorder  );
 	
-	grid.css("height" , tCellHeight + "px");
+	grid.css("min-height" , tCellHeight + "px");
 	grid.css("width" , tCellWidth + "px");
 	
 	return false;
